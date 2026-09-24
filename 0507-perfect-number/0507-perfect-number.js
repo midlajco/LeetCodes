c69de1cb19ca1e56acb4b53566a9eb072326d77f
midlajco <lapmidlaj@gmail.com>
2026-09-24 09:35:14 +0530

    Time: 2099 ms (14.23%), Space: 53.7 MB (77.94%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+
+    let sum =0;
+    for(let i=1;i<num;i++){
+
+        if(num%i==0){
+           sum =sum+i
+        }
+        
+       
+
+    }
+ if(sum==num){
+    return true;
+ }
+ else{
+   return  false
+ }
+    
+};
